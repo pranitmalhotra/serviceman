d875d52a988ee85cc8189730cc735ea3dfa26daa
PranitMalhotra <pranitmalhotra7@gmail.com>
2024-09-27 21:15:57 +0530

    chore: added ruff for linting

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 - Added [scalar](https://github.com/scalar/scalar) for api documentation
 - Added [poetry](https://github.com/python-poetry/poetry) for dependency management
-- 
+- Added [ruff](https://github.com/astral-sh/ruff) for linting
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
+
 @app.get("/scalar", include_in_schema=False)
 async def scalar_html():
     return get_scalar_api_reference(
         openapi_url=app.openapi_url,
         title=app.title,
     )
+
 
 @app.get("/health")
 def service_health():
